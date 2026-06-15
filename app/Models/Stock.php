e53563444229307560964f881057aa08db3edd07
r1mcvin <r1mc.mis.kelvinvaldez@gmail.com>
2026-06-15 20:03:51 +0800

    update

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    //
+    protected $fillable = [
+        'inventory_id',
+        'supplier_id',
+        'lot_number',
+        'manufacturer_id',
+        'date_received',
+        'received_by',
+        'expiration_date',
+        'quantity',
+        'price'
+    ]
 }
