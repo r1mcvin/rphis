19105778942fd102fb4f8e44478ec1b9a2b4ac05
r1mcvin <r1mc.mis.kelvinvaldez@gmail.com>
2026-06-15 20:01:52 +0800

    update

--- app/Models/DosageForm.php
+++ app/Models/DosageForm.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DosageForm extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'form_name'
+    ];
 }
--- database/migrations/2026_06_15_114050_create_dosage_forms_table.php
+++ database/migrations/2026_06_15_114050_create_dosage_forms_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('dosage_forms', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('form_name')->unique();
         });
     }
 
