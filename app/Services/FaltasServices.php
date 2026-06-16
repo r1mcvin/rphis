39eff75a371e5642c03afcac914371f3900bc71e
Vin Vin Valdez <124647820+r1mcvin@users.noreply.github.com>
2026-06-16 21:51:48 +0800

    update

--- app/Services/FaltasServices.php
+++ app/Services/FaltasServices.php
@@ -23,9 +23,19 @@ class FaltasServices
         $now = Carbon::now();
         // A-2606-001
         // $controlNumber = $code . '-' . $now->format('ym') . '-' . str_pad(rand(1, 999), 3, '0', STR_PAD_LEFT);
-        $controlNumber = $code . '-' . $now->format('ym') . '-0001';
+        $prefix = $code . '-' . $now->format('ym');
 
         // check if there is existing control number in the database
-        $existing_control_num = Faltas::where('control_number', 'like', $code . '-' . $now->format('ym') . '-%')->orderBy('control_number', 'desc')->first();
+        $existing_control_num = Faltas::where('control_number', 'like', $prefix . '-%')
+                                    ->orderBy('control_number', 'desc')
+                                    ->first();
+        if ($existing_control_num) {
+            $last_number = (int) substr($existing_control_num->control_number, -3);
+            $new_number = str_pad($last_number + 1, 3, '0', STR_PAD_LEFT);
+        } else {
+            $new_number = '001';
+        }
+
+        return $prefix . '-' . $new_number;
     }
 }
